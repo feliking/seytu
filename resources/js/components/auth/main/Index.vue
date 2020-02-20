62e7a9ed5504c39430261b78cb0c883b6846b4ff
felix <felixddxd@gmail.com>
2020-02-19 21:41:21 -0400

    home page

--- resources/js/components/auth/main/Index.vue
+++ resources/js/components/auth/main/Index.vue
@@ -14,26 +14,15 @@
     <v-col cols="12" class="pa-0">
       <v-carousel
         cycle
-        height="768"
+        height="600"
         hide-delimiter-background
         show-arrows-on-hover
       >
-        <v-carousel-item
-          v-for="(slide, i) in slides"
-          :key="i"
-        >
-          <v-sheet
-            :color="colors[i]"
-            height="100%"
-          >
-            <v-row
-              class="fill-height"
-              align="center"
-              justify="center"
-            >
-              <div class="display-3">{{ slide }} Slide</div>
-            </v-row>
-          </v-sheet>
+        <v-carousel-item>
+          <v-img src="/img/BannerDesktopHomeLB-C05-BIO.jpg"></v-img>
+        </v-carousel-item>
+        <v-carousel-item>
+          <v-img src="/img/BannerDesktopHomeLB-C05-BIO.jpg"></v-img>
         </v-carousel-item>
       </v-carousel>
     </v-col>
@@ -46,22 +35,63 @@
       <v-row class="pa-0">
         <v-col cols="6" class="pa-0">
           <v-img
-            src="https://i.ytimg.com/vi/y1AOmylwOvw/maxresdefault.jpg"
-            contain
-            max-height="540px">
+            src="/img/category-banner-1-c03-2020-FR-MON.jpg"
+            max-height="540px"
+            aspect-ratio="0.1">
           </v-img>
         </v-col>
         <v-col cols="6" class="pa-0">
           <v-img
-            src="https://i.ytimg.com/vi/y1AOmylwOvw/maxresdefault.jpg"
-            contain
+            src="/img/CAT-2.jpg"
             max-height="270px">
           </v-img>
           <v-img
-            src="https://i.ytimg.com/vi/y1AOmylwOvw/maxresdefault.jpg"
-            contain
+            src="/img/CAT-3.jpg"
             max-height="270px">
           </v-img>
+        </v-col>
+        <v-col cols="12">
+          <v-img
+            src="/img/banner-catalogo.jpg"
+            max-height="600px">
+          </v-img>
+        </v-col>
+        <hr>
+        <v-col class="pink lighten-5">
+          <v-row>
+            <v-col cols="2">
+
+            </v-col>
+            <v-col cols="2">
+              <p class="font-weight-bold caption">NUESTRAS MARCAS</p>
+              <p class="caption">MARCA 1</p>
+              <p class="caption">MARCA 2</p>
+              <p class="caption">MARCA 3</p>
+            </v-col>
+            <v-col cols="2">
+              <p class="font-weight-bold caption">CATALOGOS</p>
+              <p class="caption">CATALOGO 2</p>
+              <p class="caption">CATALOGO 3</p>
+              <p class="caption">CATALOGO 1</p>
+            </v-col>
+            <v-col cols="2">
+              <p class="font-weight-bold caption">LEGAL</p>
+              <p class="caption">TÉRMINOS Y CONDICIONES</p>
+              <p class="caption">CONDICIONES DE USO</p>
+              <p class="caption">POLÍTICA DE PRIVACIDAD</p>
+              <p class="caption">DERECHOS ARCO</p>
+              <p class="caption">VIGENCIA DE CAMPAÑAS</p>
+            </v-col>
+            <v-col cols="2">
+              <p class="font-weight-bold caption">CONTÁCTANOS</p>
+              <p class="caption">PREGUNTAS FRECUENTES</p>
+              <p class="caption">QUIERO VENDER</p>
+              <p class="caption">ESCRÍBENOS</p>
+            </v-col>
+            <v-col cols="2">
+
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
     </v-col>
